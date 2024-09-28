b64f7509e628ff9a2358d4f423ba5accd05f70f6
johen10 <149274310+johen10@users.noreply.github.com>
2024-09-28 14:33:51 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,17 +16,17 @@ mpl.rcParams['figure.dpi'] = 300
 import os  # <-- Add this line to import the os module
 
 # Define the path where CSV files are stored
-xlsx_directory = r'C:\\Users\\joaoc\\Documents\\Streamlit\\data'  # Make sure this directory contains your CSV files
+xlsx_directory = 'https://github.com/johen10/Streamlit-Radars.git'  # Make sure this directory contains your CSV files
 
 # Get a list of CSV files in the directory
-xlsx_files = [f for f in os.listdir(r'C:\\Users\\joaoc\\Documents\\Streamlit\\data') if f.endswith('.xlsx')]
+xlsx_files = [f for f in os.listdir('https://github.com/johen10/Streamlit-Radars.git') if f.endswith('.xlsx')]
 
 # Streamlit: Create the sidebar for file selection
 st.sidebar.title("Select Data File and Player")
 selected_file = st.sidebar.selectbox("Select XLSX File", xlsx_files)
 
 # Load the selected CSV file
-data_path = os.path.join(r'C:\\Users\\joaoc\\Documents\\Streamlit\\data', selected_file)
+data_path = os.path.join('https://github.com/johen10/Streamlit-Radars.git', selected_file)
 data = pd.read_excel(data_path)
 
 data = data.fillna(0)
